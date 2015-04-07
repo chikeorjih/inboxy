d91255dfce4888435e4d4e4fcf64ddb758ae5d29
Scott Nesham <sanesham@gmail.com>
2015-04-06 23:37:35 -0500

    this is a hack-job demo illustrating the solution to making API requests for magnet

--- src/js/components/EmailList.js
+++ src/js/components/EmailList.js
@@ -1,20 +1,54 @@
 /** @jsx React.DOM */
-var Parse       = require('parse').Parse;
 var React       = require('react');
-var ParseReact  = require('parse-react');
 var EmailTmp    = require('EmailTmp');
 
 var EmailList = React.createClass({
-  mixins: [ParseReact.Mixin],
 
-  observe: function (props, state) {
+  getInitialState: function() {
     return {
-      Emails: (new Parse.Query('Email')).ascending('recieved')
+      pictures:  [],
+      favorites: []
     };
   },
 
+  componentDidMount: function () {
+    var self = this;
+    var url = 'https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=?';
+
+    $.getJSON(url, function(result){
+
+        if(!result || !result.data || !result.data.length){
+          return;
+        }
+
+        var pictures = result.data.map(function(p){
+
+            return {
+                id: p.id,
+                url: p.link,
+                src: p.images.low_resolution.url,
+                title: p.caption ? p.caption.text : '',
+                favorite: false
+            };
+
+        });
+
+        // Update the component's state. This will trigger a render.
+        // Note that this only updates the pictures property, and does
+        // not remove the favorites array.
+
+        self.setState({ pictures: pictures });
+
+    });
+  },
+
+
   render: function(){
     var self = this;
+
+    var pictures = this.state.pictures.map(function(p){
+      return <EmailTmp ref={p.id} src={p.src} title={p.title} />
+    });
 
     return (
       <section className="email-list">
@@ -22,21 +56,11 @@ var EmailList = React.createClass({
           <h1>Inbox</h1>
         </header>
         <ul>
-          {this.data.Emails.map(function(i) {
-            return (
-              <EmailTmp key={i.id} email={i} update={self._updateItem} />
-            );
-          })}
+          {pictures}
         </ul>
       </section>
     );
   },
-
-  _updateItem: function(id, unreadIndication) {
-    ParseReact.Mutation.Set(id, {
-      unread: unreadIndication
-    }).dispatch();
-  }
 });
 
 
--- src/js/components/EmailTmp.js
+++ src/js/components/EmailTmp.js
@@ -4,63 +4,12 @@ var classSet = require('classnames');
 var moment   = require('moment');
 
 var EmailTmp = React.createClass({
-  getInitialState: function() {
-    return ({
-      unread: this.props.email.unread,
-      active: this.props.email.active
-    });
-  },
-
-  handleClick : function() {
-    if (this.state.unread) {
-      this.setState({
-        unread: false
-      }, function (){
-        this.props.update(this.props.email.id, this.state.unread);
-      });
-    }
-  },
-
-  handleFocus : function() {
-    this.setState({
-      active: true
-    }, function (){
-      this.props.update(this.props.email.id, this.state.active);
-    });
-  },
-
-  handleBlur : function() {
-    this.setState({
-      active: false
-    }, function (){
-      this.props.update(this.props.email.id, this.state.active);
-    });
-  },
+  // This component doesn't hold any state - it simply transforms
+  // whatever was passed as attributes into HTML that represents a picture.
 
   render: function(){
-
-    var indicators = classSet({
-      'indicator': true,
-      '-unread': this.state.unread,
-    });
-
-    var activeStateClass = classSet({
-      '-active': this.state.active
-    });
-
     return (
-      <li className="email">
-        <a href="#" className={activeStateClass} onClick={this.handleClick} onFocus={this.handleFocus} onBlur={this.handleBlur}>
-          <div className="-header">
-            <span className={indicators}></span>
-            <span className="sender">{this.props.email.from.name}</span>
-            <span className="date">{moment(this.props.email.recieved).format('L')}</span>
-          </div>
-          <div className="excerpt">
-          {this.props.email.body.substring(0, 50)}
-          </div>
-        </a>
-      </li>
+      <img src={this.props.src} />
     );
   }
 });
