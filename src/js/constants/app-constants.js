95d9adcdb61577e12d9d8be58e5b939eaeb03f85
Scott Nesham <sanesham@gmail.com>
2015-04-18 19:29:36 -0500

    remove all the failed attempts at application architecture

--- src/js/constants/app-constants.js
+++
@@ -1,7 +0,0 @@
-var keyMirror = require('react/lib/keyMirror');
-
-// Define action constants
-module.exports = keyMirror({
-  EMAIL_READ: null,       // Adds item to cart
-  EMAIL_DELETE: null    // Remove item from cart
-});
--- src/js/stores/app-store.js
+++
@@ -1,8 +0,0 @@
-var AppDispatcher = require('app-dispatcher');
-
-var AppStore = function () {
-  console.log("lol");
-};
-
-
-module.exports = AppStore;
